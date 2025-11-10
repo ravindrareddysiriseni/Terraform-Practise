93f4d6d0ad06e2eeb7bba13da7c737c451c7ed53
root <root@ip-10-1-1-178.ec2.internal>
2025-11-10 08:29:15 +0000

    statelocking deleted

--- remotestate.tf
+++ remotestate.tf
@@ -1,10 +1,10 @@
 terraform {
   backend "s3" {
-    bucket = "raviterraformbucket"
-    key    = "remotestate.tf"
-    region = "us-east-1"
-    dynamodb_table = "devopsbs1-terraform-state-lock"
-    encrypt        = true
+    bucket       = "raviterraformbucket"
+    key          = "remotestate.tf"
+    region       = "us-east-1"
+    use_lockfile = true
+    encrypt      = true
 
   }
 }
